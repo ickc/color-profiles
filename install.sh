99968738f34332fb1ffb13586bf247d5a7463e37
Kolen Cheung <christian.kolen@gmail.com>
2017-06-29 14:26:23 -0700

    install.sh: fix possibility of missing ColorSync dir.

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+mkdir -p ~/Library/ColorSync/Profiles/
 rm -rf ~/Library/ColorSync/Profiles
 ln -s $DIR/Profiles/ ~/Library/ColorSync/
