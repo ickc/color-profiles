876e159fe6139a67018212c736c8aeb954460087
Kolen Cheung <christian.kolen@gmail.com>
2020-01-27 19:26:35 -0800

    sync.sh: auto batch

--- sync.sh
+++ sync.sh
@@ -4,4 +4,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # local
 mkdir -p ~/Library/ColorSync/Profiles/
-unison "$DIR/Profiles/" ~/Library/ColorSync/Profiles/
+unison -auto -batch "$DIR/Profiles/" ~/Library/ColorSync/Profiles/
